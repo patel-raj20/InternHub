ad946d5a3f80462787e4a0219927d9d0148ee7e6
JAY23kanjariy <jhkanjariya4002@gmail.com>
2026-03-23 15:47:15 +0530

    update the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <div className="flex items-center justify-center h-screen">
             <h1 className="text-3xl font-bold ">
-               Welcome to <span className="animate-pulse text-blue-300">InternDesk 🚀</span>
+               Welcome to <span className="animate-pulse text-blue-300">InternHub 🚀</span>
             </h1>
         </div>
     );
